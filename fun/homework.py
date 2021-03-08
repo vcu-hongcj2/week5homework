46121d7c65d4da7fe97366df87e363c5216de536
vcu-hongcj2 <78453136+vcu-hongcj2@users.noreply.github.com>
2021-03-07 19:44:26 -0500

    Update homework.py

--- fun/homework.py
+++ fun/homework.py
@@ -1,11 +1,4 @@
-"""Homework file for my students to have fun with some algorithms! """
-
-
 def find_greatest_number(incoming_list):
-    """
-    Required parameter, incoming_list, should be a list.
-    Find the largest number in the list.
-    """
     if incoming_list is None or len(incoming_list) == 0:
         return 0
     current_largest = incoming_list[0]
@@ -14,12 +7,7 @@ def find_greatest_number(incoming_list):
             current_largest = num
     return current_largest
     
-
 def find_least_number(incoming_list):
-    """
-    Required parameter, incoming_list, should be a list.
-    Find the smallest/least number in the list.
-    """
     if incoming_list is None or len(incoming_list) == 0:
         return 0
     current_smallest = incoming_list[0]
@@ -28,25 +16,15 @@ def find_least_number(incoming_list):
             current_smallest = num
     return current_smallest
 
-
 def add_list_numbers(incoming_list):
-    """
-    Required parameter, incoming_list, should be a list.
-    Add all the values together and return it.
-    """
     if incoming_list is None:
         return 0
     total = 0
     for num in incoming_list:
         total = total + num
     return total
-        
 
 def longest_value_key(incoming_dict):
-    """
-    Required parameter, incoming_dict, should be a dict.
-    Find the KEY that has a value with the highest length, use the len() function
-    """
     if incoming_dict is None:
         return None
     current_longest_value = 0
